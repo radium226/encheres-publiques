a6898d29bbbcffcc8a2af9836c9ba7c82358b135
radium <r@di.um>
2018-12-26 10:23:23 +0100

    Debug what's going on

--- src/main/scala/com/github/radium226/encherespubliques/CheckForNewSales.scala
+++ src/main/scala/com/github/radium226/encherespubliques/CheckForNewSales.scala
@@ -12,32 +12,38 @@ import io.tmos.arm.ArmMethods._
 
 object CheckForNewSales extends App {
 
-  implicit val timer = IO.timer(ExecutionContext.global)
+  try {
 
-  implicit val config = ConfigFactory.parseFile(Paths.get("/etc/encheres-publiques.conf").toFile)
-  println(config.root().render())
+    implicit val timer = IO.timer(ExecutionContext.global)
 
-  import Interactions._
+    implicit val config = ConfigFactory.parseFile(Paths.get("/etc/encheres-publiques.conf").toFile)
+    println(config.root().render())
 
-  val token = config.getString("token")
+    import Interactions._
 
-  for (
-    browser   <- manage(Browser.open());
-    notifyBot <- manage(NotifyBot.start(token))
-  ) yield {
-    for {
-      scrapedLinks <- browser.browse(for {
-      _            <- go()
-      _            <- enterCriteria()
-      links        <- scrapeLinks()
-      } yield links)
-      visitedLinks <- Link.load("./links.txt")
-      linksToVisit  = scrapedLinks diff visitedLinks
-      _            <- Link.dump("./DEBUG-linksToVisit.txt", linksToVisit)
-      _            <- Link.dump("./DEBUG-visitedLinks.txt", visitedLinks)
-      _            <- Link.dump("./links.txt", scrapedLinks)
-      _            <- notifyBot.notifyLinks(linksToVisit)
-    } yield linksToVisit
-  }.failed.foreach(throw _)
+    val token = config.getString("token")
+
+    for (
+      browser <- manage(Browser.open());
+      notifyBot <- manage(NotifyBot.start(token))
+    ) yield {
+      for {
+        scrapedLinks <- browser.browse(for {
+          _ <- go()
+          _ <- enterCriteria()
+          links <- scrapeLinks()
+        } yield links)
+        visitedLinks <- Link.load("./links.txt")
+        linksToVisit = scrapedLinks diff visitedLinks
+        _ <- Link.dump("./DEBUG-linksToVisit.txt", linksToVisit)
+        _ <- Link.dump("./DEBUG-visitedLinks.txt", visitedLinks)
+        _ <- Link.dump("./links.txt", scrapedLinks)
+        _ <- notifyBot.notifyLinks(linksToVisit)
+      } yield linksToVisit
+    }.failed.foreach(throw _)
+  } catch {
+    case throwable: Throwable =>
+      throwable.printStackTrace()
+  }
 
 }
